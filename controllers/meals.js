30f0ad854b0a93d082026e2a953bf9024b9a1e24
DiegoBravoTorres <dbravo-torres@myseneca.ca>
2021-10-21 18:24:15 -0500

    organized by controllers

---
+++ controllers/meals.js
@@ -0,0 +1,22 @@
+const mealModel = require("../models/mealsList");
+
+const express = require("express");
+const router = express.Router();
+
+
+router.get("/", (req, res) => {
+    res.render("meals/home",{
+        topMeals: mealModel.getTopMeals(),
+       
+    });
+});
+
+router.get("/on-the-menu", (req, res) => {
+    res.render("meals/onTheMenu",{
+        mealCategories: mealModel.getAllMeals(),
+   
+    });
+
+});
+
+module.exports = router;
