672f7be0ffacef13f18650d47b9b837e3326a514
DiegoBravoTorres <dbravo-torres@myseneca.ca>
2021-11-29 16:08:50 -0600

    data fetched from MongoDB

--- controllers/meals.js
+++ controllers/meals.js
@@ -1,22 +1,58 @@
 const mealModel = require("../models/mealsList");
+const mongoose = require('mongoose');
 
 const express = require("express");
 const router = express.Router();
 
+
+
 router.get("/", (req, res) => {
-    res.render("meals/home",{
-        topMeals: mealModel.getTopMeals(),
-        title: "Home Page"
-       
+    mealModel.find()
+    .exec()
+    .then((meal) => {
+        meal = meal.map(value => value.toObject());
+        // Render the "home" view with the data
+        res.render("meals/home", {
+            meal,
+            title: "Home Page",
+           
+        });
     });
+
 });
 
 router.get("/on-the-menu", (req, res) => {
-    res.render("meals/onTheMenu",{
-        mealCategories: mealModel.getAllMeals(),
-   
+    mealModel.find()
+    .exec()
+    .then((meals) => {   
+    meals = meals.map(value => value.toObject());
+
+    // Arrange meals by category
+    let categories = [];
+    for (i = 0; i < meals.length; i++) {
+        let currentMeal = meals[i];
+        let categoryName = currentMeal.category;
+        let category = categories.find(c => c.category == categoryName);
+        if (!category) {
+            category = {
+                category: categoryName,
+                meal: []
+            };
+            categories.push(category);
+        }
+        category.meal.push(currentMeal);
+    }
+    
+    // Send meals by category
+        res.render("meals/onTheMenu", {
+            categories,
+            title: "Home Page",
+           
+        });
     });
+
 
 });
 
 module.exports = router;
+
--- models/mealsList.js
+++ models/mealsList.js
@@ -1,95 +1,25 @@
-var mealsList = [
-    {
-       category: "Italian",
-       mealKits:[
-           {
-                title: "Italian Pasta with Tomato with Seafood",
-                ingredients:"Imported Pasta and organic tomatoes along with other spices",
-                description: "A delicious pasta with the spirit of Italy",
-                price: 21.99,
-                cookingTime : 20,
-                servings:2, 
-                calories: 600,
-                imgURL: "italian-pasta-seafood.jpg",
-                topMeal: false
-           },
-           {
-
-                title: "Pepperoni and Salamy Sandwich",
-                ingredients:"Bread, pepperoni and imported salamy",
-                description: "Gourmet Sandwich made with the best quality products",
-                price: 14.99,
-                cookingTime : 10,
-                servings:3, 
-                calories: 400,
-                imgURL: "pepperoni-salamy-sandwich.jpg",
-                topMeal: true
-
-           },
-           {
-
-            title: "Homemade Lasagna",
-            ingredients:"Spinach, cheese, pasta layers and meat sauce",
-            description: "The original recipie from italy to your table",
-            price: 25.99,
-            cookingTime : 50,
-            servings:4, 
-            calories: 900,
-            imgURL: "homemade-lasagna.jpg",
-            topMeal: true
-
-           },
-           {
-
-            title: "Organic Pizza",
-            ingredients:"Cheery tomates, cheese, tomatoe sauce, mushrooms and flour",
-            description: "The best organic pizza you've done",
-            price: 16.99,
-            cookingTime : 45,
-            servings:2, 
-            calories: 1000,
-            imgURL: "organic-pizza.jpg",
-            topMeal: true
-
-       }
-       ]
-    },
-    {
-        category: "Breakfast",
-        mealKits:[
-            {
-                 title: "Blueberry Pancakes",
-                 ingredients:"Blueberries, butter, flour, syrup and sugar",
-                 description: "The perfect breakfast with blueberries on the top",
-                 price: 10.99,
-                 cookingTime : 40,
-                 servings:2, 
-                 calories: 400,
-                 imgURL: "blueberry-pancakes.jpg",
-                 topMeal: false
-            }
-        ]
-    }   
-]
+const mongoose = require('mongoose');
+const schema = mongoose.Schema;
 
 
-module.exports.getAllMeals = function(){
-    return mealsList;
-}
+const mealSchema = new schema({
 
-module.exports.getTopMeals = function(){
+    "title": String,
+    "ingredients":String,
+    "description": String,
+    "price": Number,
+    "cookingTime" : Number,
+    "servings":Number, 
+    "calories": Number,
+    "imgURL": String,
+    "topMeal": Boolean
 
-   // mealsList.flat(2);
-    var topMeals = [];
+});
 
-    for (var i =0; i < mealsList.length ; i++)
-    {
-        for (var y =0; y < mealsList[i].mealKits.length ; y++)
-        {
-            // console.log(mealsList[i].mealKits[y].title);
-            if (mealsList[i].mealKits[y].topMeal)
-            {topMeals.push(mealsList[i].mealKits[y]);}
-        }
-    }
-return topMeals;
-}
+
+const mealModel = mongoose.model("meals", mealSchema);
+module.exports = mealModel;
+
+
+
+
