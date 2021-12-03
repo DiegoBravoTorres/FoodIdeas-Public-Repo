3316e2a77f3dadba58ec87c6418e834ac5710423
DiegoBravoTorres <dbravo-torres@myseneca.ca>
2021-12-02 21:23:05 -0600

    update form updating images correctly

--- server.js
+++ server.js
@@ -121,7 +121,7 @@ app.use(function (err, req, res, next) {
 });
 
 // Define a port to listen to requests on.
-const HTTP_PORT = process.env.PORT || 8063;
+const HTTP_PORT = process.env.PORT || 8089;
 
 // Call this function after the http server starts listening for requests.
 function onHttpStart() {
